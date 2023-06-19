78b4cf354fba075eedcb48d98ca38c2e7155c4d2
Anshul <64695943+anshuldhariwal@users.noreply.github.com>
2023-06-19 18:37:52 +0530

    Create Solution-65%.py

---
+++ Is Palindrome/Solution-65%.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = [char.lower() for char in s if char.isalnum()]
+        return s == s[::-1]
+        
